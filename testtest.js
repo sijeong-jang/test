c6e0a30ef4db4fbf63ac9badc3e660fdedf6d7b2
sijeong-jang <135492708+sijeong-jang@users.noreply.github.com>
2023-06-04 14:15:39 +0900

    test1

---
+++ testtest.js
@@ -0,0 +1,12 @@
+var memberArray = ['aaa','bbb','ccc','ddd'];
+console.log("memberArray[2]", memberArray[2]);
+
+var memberObject = {
+    manager:'11111',
+    developer:'2222',
+    designer:'3333'
+}
+
+console.log("memberObject.designer", memberObject.designer);
+console.log("memberObject.developer", memberObject.developer);
+
